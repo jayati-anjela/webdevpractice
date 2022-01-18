44d48bb99c5169737508fd3ed958d9672944bdb6
jayati-anjela <jayati.minj01@gmail.com>
2022-01-18 21:46:37 +0530

    started javascript

---
+++ SECTION9JS/practice.js
@@ -0,0 +1,3 @@
+var inputtext = prompt("Write your tweet: ");
+var remnum = 240 - inputtext.length;
+alert("You have written " + inputtext.length + " characters. You have " + remnum + " characters remaining.")
