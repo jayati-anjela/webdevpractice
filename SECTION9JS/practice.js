632520c06f1fa0ab48778f7e467107eb350b07a4
jayati-anjela <jayati.minj01@gmail.com>
2022-01-19 20:26:36 +0530

    day 2 of JS

--- SECTION9JS/practice.js
+++ SECTION9JS/practice.js
@@ -1,3 +1,17 @@
 var inputtext = prompt("Write your tweet: ");
 var remnum = 240 - inputtext.length;
 alert("You have written " + inputtext.length + " characters. You have " + remnum + " characters remaining.")
+
+// day 2 of JS -> 20:17
+
+var name = "Juno";
+var nickname = name.slice(0, 3); // to show slice function
+alert(nickname);
+
+// tweeter character counter (140 characters)
+
+var inputtext = prompt("Write your tweet: ");
+var remnum = 240 - inputtext.length;
+var tweet = inputtext.slice(0,140);
+alert("Your tweet is: " + tweet + " any missing part goes beyond the character limit.");
+alert("You have written " + inputtext.length + " characters. You have " + remnum + " characters remaining.")
