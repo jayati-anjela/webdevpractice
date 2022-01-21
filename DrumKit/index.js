1b8301bb86f8bf00cfdff0d7d4dd97c07d8b6418
jayati-anjela <jayati.minj01@gmail.com>
2022-01-22 00:31:03 +0530

    badumtss

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,1 +1,16 @@
- 
+
+// document.querySelector("button").addEventListener("click",  function (){
+//    alert("I got clicked!");
+//
+//
+//  });
+
+var NumberOfDrumButtons = document.querySelectorAll(".drum").length; // to find out the number of elements which have class drum (i.e. the num of buttons)
+
+for(var i=0; i<NumberOfDrumButtons; i++){
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+     alert("I got clicked!");
+
+   });
+}
