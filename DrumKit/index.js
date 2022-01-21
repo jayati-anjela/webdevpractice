3b35285bb79bad5cb64bc785c82c639ea213f37d
jayati-anjela <jayati.minj01@gmail.com>
2022-01-22 02:30:54 +0530

    update at 3am

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,51 +1,66 @@
-var NumberOfDrumButtons = document.querySelectorAll(".drum").length; // to find out the number of elements which have class drum (i.e. the num of buttons)
+//  dectecting button press
 
+var NumberOfDrumButtons = document.querySelectorAll(".drum").length; // to find out the number of elements which have class drum (i.e. the num of buttons)
 
 for(var i=0; i<NumberOfDrumButtons; i++){
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function (){ // function () is called as anonymous function
 
       var buttonInnerHTML = this.innerHTML;
-      switch (buttonInnerHTML) {
-        case "w" :
-            var tom1 = new Audio("sounds/tom-1.mp3"); // this line creates html audio element
-            tom1.play();
-            break;
 
-        case "a" :
-            var tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play();
-            break;
-
-        case "d" :
-            var tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();
-            break;
-
-        case "x" :
-            var tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();
-            break;
-
-
-        case "j" :
-            var snare = new Audio("sounds/snare.mp3");
-            snare.play();
-            break;
-
-        case "k" :
-            var crash = new Audio("sounds/crash.mp3");
-            crash.play();
-            break;
-
-        case "l" :
-            var kick = new Audio("sounds/kick-bass.mp3");
-            kick.play();
-            break;
-
-        default: console.log(buttonInnerHTML);
-
-        }
+      makeSound(buttonInnerHTML);
 
    });
+}
+
+// detection keyboard press
+
+
+  addEventListener("keydown", function() {
+    makeSound(event.key)
+  });
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w" :
+        var tom1 = new Audio("sounds/tom-1.mp3"); // this line creates html audio element
+        tom1.play();
+        break;
+
+    case "a" :
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+    case "d" :
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+
+    case "x" :
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+
+    case "j" :
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+
+    case "k" :
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+
+    case "l" :
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+
+    default: console.log(key);
+
+    }
+
 }
