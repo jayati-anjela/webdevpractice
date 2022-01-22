71b6600019aa203922ffb055276f90afc316960d
jayati-anjela <jayati.minj01@gmail.com>
2022-01-22 23:10:38 +0530

    update

--- jQuery/index.js
+++ jQuery/index.js
@@ -6,5 +6,10 @@
 
 // the above code is not used if your js and jquery scripts are placed at the bottom bec they work the same way
 
- $("h1").addClass("big-title");
- 
+
+
+/* to add one class
+ $("h1").addClass("big-title"); */
+
+// add multiple classes
+ $("h1").addClass("big-title marginn");
