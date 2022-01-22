578d0a1e744e9bde8e40d9d31f5e202bba923f19
jayati-anjela <jayati.minj01@gmail.com>
2022-01-22 23:01:18 +0530

    slow day

---
+++ jQuery/index.js
@@ -0,0 +1,7 @@
+// function to see if our jQuery is working
+
+// $(document).ready( function() {
+   // here we put all code
+// });
+
+// the above code is not used if your js and jquery scripts are placed at the bottom bec they work the same way
