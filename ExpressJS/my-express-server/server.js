cc023315a06845a2d570f397885cab3d55447d8c
jayati-anjela <jayati.minj01@gmail.com>
2022-01-24 22:24:22 +0530

    xoxo

--- ExpressJS/my-express-server/server.js
+++ ExpressJS/my-express-server/server.js
@@ -2,6 +2,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", function(request, response) {
+  response.send("Hello");
+});
+
 app.listen(3000, function (){
   console.log("Server started on port 3000");
 });
