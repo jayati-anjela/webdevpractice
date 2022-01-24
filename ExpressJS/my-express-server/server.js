1c695a96a3268281353500e571bda5e5af0b2549
jayati-anjela <jayati.minj01@gmail.com>
2022-01-24 22:25:53 +0530

    uhh

--- ExpressJS/my-express-server/server.js
+++ ExpressJS/my-express-server/server.js
@@ -6,6 +6,10 @@ app.get("/", function(request, response) {
   response.send("Hello");
 });
 
+app.get("/about", function(request, response) {
+  response.send("I am a prefinal student currently dabbling in various bits such as web development, finance, UI/UX design, graphic design, and personal-development books!");
+});
+
 app.listen(3000, function (){
   console.log("Server started on port 3000");
 });
