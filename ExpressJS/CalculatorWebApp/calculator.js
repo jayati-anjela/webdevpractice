cc23d8845e9b6aeac0700a304bbeeda18e0c84d9
jayati-anjela <jayati.minj01@gmail.com>
2022-01-24 23:24:44 +0530

    old course causes so much problem :/

--- ExpressJS/CalculatorWebApp/calculator.js
+++ ExpressJS/CalculatorWebApp/calculator.js
@@ -1,9 +1,19 @@
 
 const express = require("express");
+const fs = require("fs");
+
 const app = express();
 
+let indexHTML = fs.readFileSync("index.html", "utf-8");
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
-  res.send("Hello World");
+  res.status(400).send(indexHTML); // status(400) mean if there is any problem this status will show
+});
+
+app.post("/", function(req, res) {
+  res.send("Thanks for posting that!");
+  console.log(req.body);
 });
 
 app.listen(3000, function (){
