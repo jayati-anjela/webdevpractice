03f5f3ac6e2ee70586c0466b71bfea3a90382317
jayati-anjela <jayati.minj01@gmail.com>
2022-01-26 00:10:12 +0530

    last update maybe

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -5,6 +5,9 @@
 
  const app = express();
 
+ // parsing using express
+ app.use(express.urlencoded({entended: true}));
+
 
  app.get("/", function(req, res){
 
@@ -13,35 +16,32 @@
 
  app.post("/", function(req, res) {
 
+   // in lec 246 we divide the url into consts
+   const query = req.body.cityName // here cityName is the token we get from our html (client side)
+   const APIkey = ""
+   const unit = "metric"
+   // these key and values replace the parameters in the url
+   const url = "url of the website";
+
+   https.get(url, function(response) {
+     console.log(response.statusCode);
+
+     response.on("data", function(data){
+       const weatherData = JSON.parse(data);
+       const temp = weatherData.main.temp;
+       const weatherDescription = weatherData.weather[0].description;
+       res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
+       res.write("It is currently a " + weatherDescription + "weather");
+
+       const icon = weatherData.weather[0].icon;
+       const imageURL = "url (endpoint)" + icon + "@2x.png";
+       res.write("<img src=" + imageURL + ">");
+       res.send();
+
+     })
+   })
  })
 
  app.listen(3000, function() {
    console.log("Server is running on port 3000.")
  });
-/*
- // in lec 246 we divide the url into consts
- const query = ""
- const APIkey = ""
- const unit = "metric"
- // these key and values replace the parameters in the url
- const url = "url of the website";
-
- https.get(url, function(response) {
-   console.log(response.statusCode);
-
-   response.on("data", function(data){
-     const weatherData = JSON.parse(data);
-     const temp = weatherData.main.temp;
-     const weatherDescription = weatherData.weather[0].description;
-     res.write("<h1>The temperature in Delhi is " + temp + " degrees Celcius.</h1>");
-     res.write("It is currently a " + weatherDescription + "weather");
-
-     const icon = weatherData.weather[0].icon;
-     const imageURL = "url (endpoint)" + icon + "@2x.png";
-     res.write("<img src=" + imageURL + ">");
-     res.send();
-
-   })
- })
-
- */
