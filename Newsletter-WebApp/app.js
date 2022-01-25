4aaf3266d4b97ee4e18ddf935755a5612c252f5c
jayati-anjela <jayati.minj01@gmail.com>
2022-01-26 01:39:30 +0530

    yuh

--- Newsletter-WebApp/app.js
+++ Newsletter-WebApp/app.js
@@ -4,9 +4,13 @@ const https = require("https");
 const app = express();
 
 // parsing using express
-app.use(express.urlencoded({entended: true}));
+app.use(express.urlencoded({extended: true}));
+// sets folder public as root (?) directory
+app.use(express.static(__dirname));
 
-
+app.get("/", function(req,res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
 
 
