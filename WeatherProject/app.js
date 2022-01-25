fe11e1af4c7cd26553155ce1f628f828cf4b1470
jayati-anjela <jayati.minj01@gmail.com>
2022-01-25 23:08:37 +0530

    break

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -7,18 +7,41 @@
 
 
  app.get("/", function(req, res){
-   const url = "url of the website";
 
-   https.get(url, function(response) {
-     console.log(response);
-
-     response.on("data", function(data){
-       console.log(data);
-     })
-   })
-   res.send("Server is up and running!")
+   res.sendFile(__dirname + "index.html");
  });
+
+ app.post("/", function(req, res) {
+
+ })
 
  app.listen(3000, function() {
    console.log("Server is running on port 3000.")
  });
+/*
+ // in lec 246 we divide the url into consts
+ const query = ""
+ const APIkey = ""
+ const unit = "metric"
+ // these key and values replace the parameters in the url
+ const url = "url of the website";
+
+ https.get(url, function(response) {
+   console.log(response.statusCode);
+
+   response.on("data", function(data){
+     const weatherData = JSON.parse(data);
+     const temp = weatherData.main.temp;
+     const weatherDescription = weatherData.weather[0].description;
+     res.write("<h1>The temperature in Delhi is " + temp + " degrees Celcius.</h1>");
+     res.write("It is currently a " + weatherDescription + "weather");
+
+     const icon = weatherData.weather[0].icon;
+     const imageURL = "url (endpoint)" + icon + "@2x.png";
+     res.write("<img src=" + imageURL + ">");
+     res.send();
+
+   })
+ })
+
+ */
