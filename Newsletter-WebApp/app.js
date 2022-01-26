8abbe9d0a19217752236296b651423aa6147e7f0
jayati-anjela <jayati.minj01@gmail.com>
2022-01-26 14:12:29 +0530

    almost finished

--- Newsletter-WebApp/app.js
+++ Newsletter-WebApp/app.js
@@ -14,12 +14,59 @@ app.get("/", function(req,res) {
 
 app.post("/", function(req,res) {
 
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var emailid = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const emailid = req.body.email;
 
-  console.log(firstName, lastName, emailid);
+
+  const data = {
+    members: [
+      {
+        email_address: emailid,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data); // turns data into json format
+  const server = "us20";
+  const audienceID = "86b6d1e0ff";
+  const url = "https://" + server + ".api.mailchimp.com/3.0/lists/" + audienceID;
+
+  const options = {
+    method: "POST",
+    auth: "juno1:443ebf98b682926e8db7865fd46d2ab7-us20"
+  };
+
+const request = https.request(url, options, function (response) {
+
+  if(response.statusCode === 200){
+    res.send("Successfully subscribed!");
+  } else {
+    res.send("There was an error with signing up, please try again!");
+  }
+
+  response.on("data", function (data) {
+    console.log(JSON.parse(data));
+  });
 });
+
+request.write(jsonData);
+request.end();
+});
+
+
+
+
+
+/*
+API KEY -> 443ebf98b682926e8db7865fd46d2ab7-us20 */
+/*
+LIST ID -> 86b6d1e0ff */
 
 
 
